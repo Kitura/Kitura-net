a7c8b6cc82711b9867cc2ff7720ec0d500c4e6c8
Robert F. Dickerson <rfdickerson@gmail.com>
2016-01-08 15:10:24 -0600

    fixed issue with strings by adding bridge

--- HttpParser/UrlParser.swift
+++ HttpParser/UrlParser.swift
@@ -77,9 +77,9 @@ public class UrlParser : CustomStringConvertible {
             }
             
             if let query = query {
-                let pairs = query.componentsSeparatedByString("&")
+                let pairs = query.bridge().componentsSeparatedByString("&")
                 for pair in pairs {
-                    let pairArr = pair.componentsSeparatedByString("=")
+                    let pairArr = pair.bridge().componentsSeparatedByString("=")
                     if pairArr.count == 2 {
                         queryParams[pairArr[0]] = pairArr[1]
                     }
