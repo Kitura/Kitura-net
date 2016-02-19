84874b9c2137e19d7b06b8bcc8d8da37cb0a36e8
rolivieri <roliv@us.ibm.com>
2016-02-19 15:28:32 -0600

    Updated dependency version numbers.

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura-sys.git", versions: Version(0,2,0)..<Version(0,3,0)),
         //.Package(url: "git@github.com:IBM-Swift/Kitura-sys.git", majorVersion: 0),
-        .Package(url: "https://github.com/IBM-Swift/BlueSocket.git", versions: Version(0,2,0)..<Version(0,3,0)),
+        .Package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 0),
         //.Package(url: "git@github.com:IBM-Swift/BlueSocket.git", majorVersion: 0),
         .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", versions: Version(0,2,0)..<Version(0,3,0)),
         //.Package(url: "git@github.com:IBM-Swift/LoggerAPI.git", majorVersion: 0),
