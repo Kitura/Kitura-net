6e5f691b450db59585c2c020b80141047a1e6f54
Robert F. Dickerson <rfdickerson@gmail.com>
2016-02-28 09:23:36 -0600

    Updated to development version of helpers
    IBM-Swift/Kitura#175

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura-sys.git", versions: Version(0,3,0)..<Version(0,4,0)),
         .Package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 0),
-        .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", versions: Version(0,2,0)..<Version(0,3,0)),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-CurlHelpers.git", versions: Version(0,2,0)..<Version(0,3,0)),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-HttpParserHelper.git", versions: Version(0,2,0)..<Version(0,3,0))
+        .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", versions: Version(0,3,0)..<Version(0,4,0)),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-CurlHelpers.git", versions: Version(0,3,0)..<Version(0,4,0)),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-HttpParserHelper.git", versions: Version(0,3,0)..<Version(0,4,0))
     ]
 )
