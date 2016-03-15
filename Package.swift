beb61db17e8df582bd9f90776996a9bc07938681
Daniel Firsht <dfirsht@us.ibm.com>
2016-03-15 16:25:33 -0500

    IBM-Swift/Kitura#284 explicitly mark support for BlueSocket v0.1

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     name: "Kitura-net",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura-sys.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 0),
+        .Package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/IBM-Swift/CCurl.git", majorVersion: 0, minor: 0),
         .Package(url: "https://github.com/IBM-Swift/CHttpParser.git", majorVersion: 0, minor: 0),
