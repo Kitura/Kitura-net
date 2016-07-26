51adf53d950c5f3ed18d36c31e18accf0155ccb4
dsperling <dsperling@smithmicro.com>
2016-07-26 08:36:30 -0700

    workaround for SR-1901/SR-2170

--- Sources/KituraNet/HTTPParser/URLParser.swift
+++ Sources/KituraNet/HTTPParser/URLParser.swift
@@ -18,7 +18,7 @@ import Foundation
 
 
 // MARK: URLParser
-
+#if SR_1901_BUG
 public class URLParser : NSURL {
 
     /// 
@@ -31,7 +31,7 @@ public class URLParser : NSURL {
     ///
     /// The userid and password if specified in the URL
     ///
-    @available(*, deprecated: 0.23, message: "use user or password attributes instead")
+    @available(*, deprecated: 0.23, message: "use user and password attributes instead")
     
     public var userinfo: String? {
         // both are specified
@@ -100,3 +100,122 @@ public class URLParser : NSURL {
     }
 }
 
+#endif
+
+public class URLParser : CustomStringConvertible {
+
+    private var url: NSURL?
+
+    ///
+    /// Schema
+    ///
+    public var schema: String? {
+        return url?.scheme
+    }
+
+    ///
+    /// Hostname
+    ///
+    public var host: String? {
+        return url?.host
+    }
+
+    ///
+    /// Path portion of the URL
+    ///
+    public var path: String? {
+        return url?.path
+    }
+
+    ///
+    /// The entire query portion of the URL
+    ///
+    public var query: String? {
+        return url?.query
+    }
+
+    ///
+    /// TThe fragment
+    ///
+    public var fragment: String? {
+        return url?.fragment
+    }
+
+    ///
+    /// The userid and password if specified in the URL
+    ///
+    @available(*, deprecated: 0.23, message: "use user and password attributes instead")
+
+    public var userinfo: String? {
+        // both are specified
+        if let username = url?.user, let password = url?.password {
+            return username + ":" + password
+        }
+        // username only
+        if let username = url?.user {
+            return username
+        }
+        // password only
+        if let password = url?.user {
+            return ":" + password
+        }
+        return nil
+    }
+
+    public var user: String? {
+        return url?.user
+    }
+
+    public var password: String? {
+        return url?.password
+    }
+
+    ///
+    /// The port specified, if any, in the URL
+    ///
+    public var port: UInt16? {
+        return url?.port?.uint16Value
+    }
+
+    ///
+    /// The query parameters brokenn out
+    ///
+    public var queryParameters: [String:String] = [:]
+
+    ///
+    /// Nicely formatted description of the parsed result
+    ///
+    public var description: String {
+        if let description = url?.description {
+            return description
+        }
+        return ""
+    }
+
+
+    ///
+    /// Initializes a new URLParser instance
+    ///
+    /// - Parameter url: url to be parsed
+    /// - Parameter isConnect: whether or not a connection has been established
+    ///
+    public init (url data: NSData, isConnect: Bool) {
+
+        #if os(Linux)
+        url = NSURL(string: String(data: data, encoding: NSUTF8StringEncoding) ?? "", relativeToURL: nil)
+        #else
+        url = NSURL(string: String(data: data as Data, encoding: String.Encoding.utf8) ?? "", relativeTo: nil)
+        #endif
+        if let query = url?.query {
+
+            let pairs = query.components(separatedBy: "&")
+            for pair in pairs {
+
+                let pairArray = pair.components(separatedBy: "=")
+                if pairArray.count == 2 {
+                    queryParameters[pairArray[0]] = pairArray[1]
+                }
+            }
+        }
+    }
+}
