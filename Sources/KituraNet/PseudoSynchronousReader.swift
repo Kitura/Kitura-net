fcaf544b4473c873bb009f7de165c7ca56ff8d38
Shmuel Kallner <kallner@il.ibm.com>
2016-07-26 16:27:21 +0300

    IBM-Swift/Kitura#435 Eliminated extraneous fields. Changed field names, added functions, and added comments to make the code more understandable

--- Sources/KituraNet/PseudoSynchronousReader.swift
+++ Sources/KituraNet/PseudoSynchronousReader.swift
@@ -20,21 +20,35 @@ import Foundation
 import LoggerAPI
 import Socket
 
+/// The PseudoSynchronousReader class provides an "adapter" between the asynchronous
+/// arival of data on the socket and the synchronous read APIs in the upper API layers.
+/// Reads from this "adapter", read all available data in it's internal buffer.
+///
+/// To do this, this class maintains an internal data buffer, a lock, and a semaphore.
+///
+/// As both the addDataToRead and the read functions modify the internal data buffer (either
+/// adding data to it or emptying it out), they both grab the bufferLock before modifying
+/// the buffer. An optimization is performed in the addDataToRead, in that if the buffer is
+/// empty the lock isn't acquired.
+///
+/// When the data buffer is empty the read function uses the readingSemaphore to wait, if
+/// needed, for data to be added to the buffer.
+///
+/// **Note:** The Grand Central Dispatch DispatchSemaphore class can act as a classic
+/// semaphore and as a lock.
 class PseudoSynchronousReader {
 
     private let clientSocket: Socket
-    private var errorFlag = false
     
-    private var inputNotAvailable = true
-    private var socketNotClosed = true
+    private var noDataToRead = true
     private let buffer = NSMutableData()
 
     #if os(Linux)
-        private let waitingForInput: dispatch_semaphore_t
-        private let readBufferLock: dispatch_semaphore_t
+        private let readingSemaphore: dispatch_semaphore_t
+        private let bufferLock: dispatch_semaphore_t
     #else
-        private let waitingForInput = DispatchSemaphore(value: 0)
-        private let readBufferLock = DispatchSemaphore(value: 1)
+        private let readingSemaphore = DispatchSemaphore(value: 0)
+        private let bufferLock = DispatchSemaphore(value: 1)
     #endif
     
     var remoteHostname: String { return clientSocket.remoteHostname }
@@ -43,19 +57,18 @@ class PseudoSynchronousReader {
         self.clientSocket = clientSocket
         
         #if os(Linux)
-            waitingForInput = dispatch_semaphore_create(0)
-            readBufferLock = dispatch_semaphore_create(1)
+            readingSemaphore = dispatch_semaphore_create(0)
+            bufferLock = dispatch_semaphore_create(1)
         #endif
     }
     
+    /// Add data to the internal buffer to be read by the raed function
+    ///
+    /// - Parameter from: The NSData object containing the data to be added
     func addDataToRead(from: NSData) {
         let needToLock = buffer.length != 0
         if  needToLock {
-            #if os(Linux)
-                dispatch_semaphore_wait(readBufferLock, DISPATCH_TIME_FOREVER)
-            #else
-                _ = readBufferLock.wait(timeout: DispatchTime.distantFuture)
-            #endif
+            lockReadLock()
         }
         
         #if os(Linux)
@@ -64,61 +77,66 @@ class PseudoSynchronousReader {
             buffer.append(from as Data)
         #endif
         
-        if  inputNotAvailable  {
+        if  noDataToRead  {
             #if os(Linux)
-                dispatch_semaphore_signal(waitingForInput)
+                dispatch_semaphore_signal(readingSemaphore)
             #else
-                waitingForInput.signal()
+                readingSemaphore.signal()
             #endif
-            inputNotAvailable = false
+            noDataToRead = false
         }
         if  needToLock  {
-            #if os(Linux)
-                dispatch_semaphore_signal(readBufferLock)
-            #else
-                readBufferLock.signal()
-            #endif
+            unlockReadLock()
         }
     }
     
+    /// Read data from the buffer to the specified NSMutableData
+    ///
+    /// - Parameter into: The NSMutableData object to append the data in the buffer to.
     func read(into: NSMutableData) -> Int {
-        if  inputNotAvailable  {
+        if  noDataToRead  {
             #if os(Linux)
-                dispatch_semaphore_wait(waitingForInput, DISPATCH_TIME_FOREVER)
+                dispatch_semaphore_wait(readingSemaphore, DISPATCH_TIME_FOREVER)
             #else
-                _ = waitingForInput.wait(timeout: DispatchTime.distantFuture)
+                _ = readingSemaphore.wait(timeout: DispatchTime.distantFuture)
             #endif
         }
         let result: Int
         if  buffer.length != 0  {
+            lockReadLock()
+            
+            result = buffer.length
             #if os(Linux)
-                dispatch_semaphore_wait(readBufferLock, DISPATCH_TIME_FOREVER)
+                into.append(buffer)
             #else
-                _ = readBufferLock.wait(timeout: DispatchTime.distantFuture)
+                into.append(buffer as Data)
             #endif
-            if  errorFlag  {
-                result = -1
-            }
-            else {
-                result = buffer.length
-                #if os(Linux)
-                    into.append(buffer)
-                #else
-                    into.append(buffer as Data)
-                #endif
-            }
+            
             buffer.length = 0
-            inputNotAvailable = socketNotClosed
-            #if os(Linux)
-                dispatch_semaphore_signal(readBufferLock)
-            #else
-                readBufferLock.signal()
-            #endif
+            noDataToRead = true
+            
+            unlockReadLock()
         }
         else {
             result = 0
         }
         
         return result
+    }
+    
+    private func lockReadLock() {
+        #if os(Linux)
+            dispatch_semaphore_wait(bufferLock, DISPATCH_TIME_FOREVER)
+        #else
+            _ = bufferLock.wait(timeout: DispatchTime.distantFuture)
+        #endif
+    }
+    
+    private func unlockReadLock() {
+        #if os(Linux)
+            dispatch_semaphore_signal(bufferLock)
+        #else
+            bufferLock.signal()
+        #endif
     }
 }
