90e4ac84cbda51865f0b537ac2d165ad0dfae74c
Shmuel Kallner <kallner@il.ibm.com>
2016-08-26 16:50:58 +0300

    IBM-Swift/Kitura#675 Workaround for SPM bug

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura-sys.git", Version(0, 27, 1)),
         .Package(url: "https://github.com/IBM-Swift/BlueSocket.git", Version(0, 8, 11)),
-        .Package(url: "https://github.com/IBM-Swift/CCurl.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/IBM-Swift/CCurl.git", Version(0, 2, 1)),
         .Package(url: "https://github.com/IBM-Swift/CHTTPParser.git", majorVersion: 0, minor: 3),
     ]
 )
