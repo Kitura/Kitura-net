d3ae47c93ccf81b4cd05f795e910a11058b99b30
Shmuel Kallner <kallner@il.ibm.com>
2016-09-04 15:28:44 +0300

    IBM-Swift/Kitura#628 Implemented most of the recommended changes from the review.

--- Sources/KituraNet/BufferList.swift
+++ Sources/KituraNet/BufferList.swift
@@ -18,12 +18,18 @@ import Foundation
 
 // MARK: BufferList 
 
+/// This class provides an implementation of a buffer that can be added to and
+/// "taken" from in chunks. Data is always added to the end of the buffer and
+/// data is "taken" from the buffer from the beginning towards the end. A pointer
+/// is maintained as where the next chunk of data will be "taken" from. This
+/// pointer can be reset, to enable the data in the buffer to be fetched from the
+/// beginning again.
 public class BufferList {
 
     // MARK: -- Private 
     
     /// Internal storage buffer
-    private let localData = NSMutableData()
+    private let localData = NSMutableData(capacity: 4096) ?? NSMutableData()
     
     /// Byte offset inside of internal storage buffer
     private var byteIndex = 0
@@ -64,16 +70,12 @@ public class BufferList {
     ///
     /// - Parameter array: a [UInt8] for the data you want from the buffer
     ///
-    /// - Returns:
+    /// - Returns: The number of bytes actually copied from the buffer. It will be the
+    ///           lessor of the number of bytes left in the buffer and the length
+    ///           of the array.
     public func fill(array: inout [UInt8]) -> Int {
         
-        let result = min(array.count, localData.length - byteIndex)
-        let bytes = localData.bytes.assumingMemoryBound(to: UInt8.self) + byteIndex
-        UnsafeMutableRawPointer(mutating: array).copyBytes(from: bytes, count: result)
-        byteIndex += result
-        
-        return result
-        
+        return fill(buffer: UnsafeMutablePointer(mutating: array), length: array.count)
     }
     
     /// Fill memory with data from the buffer
@@ -81,7 +83,9 @@ public class BufferList {
     /// - Parameter buffer: NSMutablePointer to the beginning of the memory to be filled
     /// - Parameter length: The number of bytes to fill
     ///
-    /// - Returns:
+    /// - Returns: The number of bytes actually copied from the buffer. It will be the
+    ///           lessor of the number of bytes left in the buffer and the length
+    ///           of the memory area provided.
     public func fill(buffer: UnsafeMutablePointer<UInt8>, length: Int) -> Int {
         
         let result = min(length, localData.length - byteIndex)
@@ -97,7 +101,8 @@ public class BufferList {
     ///
     /// - Parameter data: The Data struct to fill from data in the buffer
     ///
-    /// - Returns:
+    /// - Returns: The number of bytes actually copied from the buffer. It will be equal
+    ///           to the number of bytes lect in the buffer.
     public func fill(data: inout Data) -> Int {
         
         let result = localData.length - byteIndex
@@ -111,7 +116,8 @@ public class BufferList {
     ///
     /// - Parameter data: The NSMutableData object to fill from data in the buffer
     ///
-    /// - Returns:
+    /// - Returns: The number of bytes actually copied from the buffer. It will be equal
+    ///           to the number of bytes lect in the buffer.
     public func fill(data: NSMutableData) -> Int {
         
         let result = localData.length - byteIndex
@@ -129,7 +135,8 @@ public class BufferList {
         
     }
     
-    /// Sets the buffer back to the beginning position
+    /// Sets the buffer back to the beginning position, the next fill call will take data
+    /// from the beginning of the buffer.
     public func rewind() {
         
         byteIndex = 0
