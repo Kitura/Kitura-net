4151d1131939453cfcf03edfd688b9f5e956e78d
Shmuel Kallner <kallner@il.ibm.com>
2016-09-08 19:51:30 +0300

    Automatically submitted PR for migration to 0.30.0 (#102)
    * updated dependency versions in Package.swift
    
    set version of https://github.com/IBM-Swift/Kitura-sys.git to 0.30.0
    
    * DEVELOPMENT-SNAPSHOT-2016-09-07-a

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import PackageDescription
 let package = Package(
     name: "Kitura-net",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura-sys.git", majorVersion: 0, minor: 28),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-sys.git", majorVersion: 0, minor: 30),
         .Package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 0, minor: 10),
         .Package(url: "https://github.com/IBM-Swift/CCurl.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/IBM-Swift/CHTTPParser.git", majorVersion: 0, minor: 3),
