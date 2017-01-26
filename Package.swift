03808bee2b5cf56a6e0129728c5953753e0e2719
Shmuel Kallner <kallner@il.ibm.com>
2017-01-26 13:14:42 +0200

    IBM-Swift/Kitura#974 Deleted CHTTPParser and updated dependency

--- Package.swift
+++ Package.swift
@@ -18,13 +18,10 @@ import PackageDescription
 
 let package = Package(
     name: "Kitura-net",
-    targets: [
-        Target(name: "KituraNet", dependencies: ["CHTTPParser"]),
-        Target(name: "CHTTPParser", dependencies: [])
-    ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 1, minor: 5),
+        .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 1, minor: 6),
         .Package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 0, minor: 12),
+        .Package(url: "https://github.com/IBM-Swift/CHTTPParser.git", majorVersion: 0, minor: 3),
         .Package(url: "https://github.com/IBM-Swift/CCurl.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/IBM-Swift/BlueSSLService.git", majorVersion: 0, minor: 12)
     ]
