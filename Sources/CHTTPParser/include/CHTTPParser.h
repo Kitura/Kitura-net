763b5e3acbccd71f764b8aa993de416b6f3f9a73
Youming Lin <youminglin@outlook.com>
2017-06-01 12:58:10 -0500

    Remove modulemap; add CHTTPParser.h

---
+++ Sources/CHTTPParser/include/CHTTPParser.h
@@ -0,0 +1,15 @@
+//
+//  CHTTPParser.h
+//  Kitura-net
+//
+//  Created by Youming Lin on 6/1/17.
+//
+//
+
+#ifndef CHTTPParser_h
+#define CHTTPParser_h
+
+#include "http_parser.h"
+#include "utils.h"
+
+#endif /* CHTTPParser_h */
