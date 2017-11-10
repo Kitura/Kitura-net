89331a82a879d43b84fe2426383af59c86dfc575
David Jones <djones6@uk.ibm.com>
2017-11-10 11:40:15 +0000

    Refactor parser error state to separate variable, for clarity

--- Sources/KituraNet/HTTP/IncomingHTTPSocketProcessor.swift
+++ Sources/KituraNet/HTTP/IncomingHTTPSocketProcessor.swift
@@ -49,11 +49,14 @@ public class IncomingHTTPSocketProcessor: IncomingSocketProcessor {
     ///HTTP Parser
     private let httpParser: HTTPParser
     
+    /// Indicates whether the HTTP parser has encountered a parsing error
+    private var parserErrored = false
+    
     /// Controls the number of requests that may be sent on this connection.
     private(set) var keepAliveState: KeepAliveState
     
     /// Should this socket actually be kept alive?
-    var isKeepAlive: Bool { return clientRequestedKeepAlive && keepAliveState.keepAlive() }
+    var isKeepAlive: Bool { return clientRequestedKeepAlive && keepAliveState.keepAlive() && !parserErrored }
     
     let socket: Socket
     
@@ -201,7 +204,7 @@ public class IncomingHTTPSocketProcessor: IncomingSocketProcessor {
             response.statusCode = .badRequest
             // We must avoid any further attempts to process data from this client
             // after a parser error has occurred. (see Kitura-net#228)
-            clientRequestedKeepAlive = false
+            parserErrored = true
             do {
                 try response.end()
             }
