0fb42de8358eff560d2e6a7faf98ebeea32f343b
ShihabMehboob <shihab.mehboob1@ibm.com>
2018-02-22 21:35:22 +0000

    Fix logger not found

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@
 import PackageDescription
 
 var dependencies: [Package.Dependency] = [
-    .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", .upToNextMinor(from: "2.0.0")),
+    .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", .upToNextMajor(from: "1.7.0")),
     .package(url: "https://github.com/IBM-Swift/BlueSocket.git", .upToNextMinor(from: "0.12.0")),
     .package(url: "https://github.com/IBM-Swift/CCurl.git", .upToNextMinor(from: "0.5.0")),
     .package(url: "https://github.com/IBM-Swift/BlueSSLService.git", .upToNextMinor(from: "0.12.0"))
