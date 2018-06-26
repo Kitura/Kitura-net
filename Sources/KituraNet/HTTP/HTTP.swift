2d7b570ce27a8d65a4e5282e1bc7fcbaf65ef72f
Kye Maloy <kyemaloy2013@gmail.com>
2018-06-26 11:37:55 +0100

    Added back the copyright for Swift.org

--- Sources/KituraNet/HTTP/HTTP.swift
+++ Sources/KituraNet/HTTP/HTTP.swift
@@ -12,7 +12,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * This source file includes content derived from the Swift.org Server APIs open source project
  */
+
+
 
 import Foundation
 
