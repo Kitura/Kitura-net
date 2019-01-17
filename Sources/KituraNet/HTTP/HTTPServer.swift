048515cafdd34de8843e52bdaaa16557cd27fd3a
David Jones <djones6@uk.ibm.com>
2019-01-17 10:22:30 +0000

    Fix additional instance

--- Sources/KituraNet/HTTP/HTTPServer.swift
+++ Sources/KituraNet/HTTP/HTTPServer.swift
@@ -492,7 +492,7 @@ public class HTTPServer: Server {
                 response.statusCode = .notFound
                 let theBody = "Path not found"
                 response.headers["Content-Type"] = ["text/plain"]
-                response.headers["Content-Length"] = [String(theBody.lengthOfBytes(using: .utf8))]
+                response.headers["Content-Length"] = [String(theBody.utf8.count)]
                 try response.write(from: theBody)
                 try response.end()
             }
